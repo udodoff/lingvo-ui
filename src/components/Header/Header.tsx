fee3d88678fb265dfc3c46d22140909f9ad94c7f
udodoff <udodovr03@gmail.com>
2024-03-27 16:22:34 +0300

    Add layout container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,36 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
 import { Button } from '@components/Button/Button';
+import { Container } from '@components/Container/Container';
 import styles from './Header.module.scss';
 import Logo from '@assets/images/logo.svg';
 
-import { FC } from 'react';
-
 export const Header: FC = () => {
     return (
-        <nav className={styles.header}>
-            <Image src={Logo} alt="Логотип" className={styles.logo} />
-            <ul className={styles.navList}>
-                <li>Главная</li>
-                <li>Обучение</li>
-                <li>Тренажеры</li>
-                <li>Разговорный клуб</li>
-                <li>О нас</li>
-                <li>Организациям</li>
-                <li>Контакты</li>
-            </ul>
-            <div className={styles.buttons}>
-                <Button theme="primary" variant="filled">
-                    Вход
-                </Button>
-                <Button theme="primary" variant="filled">
-                    Регистрация
-                </Button>
-            </div>
-        </nav>
+        <Container>
+            <nav className={styles.header}>
+                <Link href="/">
+                    <Image src={Logo} alt="Логотип" className={styles.logo} />
+                </Link>
+                <ul className={styles.navList}>
+                    <li>Главная</li>
+                    <li>Обучение</li>
+                    <li>Тренажеры</li>
+                    <li>Разговорный клуб</li>
+                    <li>О нас</li>
+                    <li>Организациям</li>
+                    <li>Контакты</li>
+                </ul>
+                <div className={styles.buttons}>
+                    <Button theme="primary" variant="filled">
+                        Вход
+                    </Button>
+                    <Button theme="primary" variant="filled">
+                        Регистрация
+                    </Button>
+                </div>
+            </nav>
+        </Container>
     );
 };
